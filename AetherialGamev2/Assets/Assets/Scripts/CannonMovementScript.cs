6f399c68124b67c70ae652faeb7f421d55c41658
adenmark <oskar_ade_95@hotmail.se>
2018-02-07 15:04:31 +0100

    first update marcus build
    first upload of marcus build from oskar

---
+++ AetherialGamev2/Assets/Assets/Scripts/CannonMovementScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonMovementScript : MonoBehaviour
+{
+    public float speed = 5f;        //Speed at which the Cannon turns
+
+    private void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;       // Direction between our position and target position, since mouse we use ScreenToWorldPoint
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;                                // Calculate the Angle
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);                                 // Create a roation from the Angle
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);        // Begin the rotation according to the speed
+    }
+}
---
+++ AetherialGamev2/Assets/Assets/Scripts/HarponScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarponScript : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D body;
+    public float timer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
