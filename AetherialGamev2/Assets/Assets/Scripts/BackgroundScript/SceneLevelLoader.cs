904beac4d8c3823ef4fcc681fbf51401d4257cbe
adenmark <oskar_ade_95@hotmail.se>
2018-02-21 14:59:48 +0100

    working newscene loader
    fixed the scnene loader

--- AetherialGamev2/Assets/Assets/Scripts/BackgroundScript/SceneLevelLoader.cs
+++ AetherialGamev2/Assets/Assets/Scripts/BackgroundScript/SceneLevelLoader.cs
@@ -7,24 +7,29 @@ public class SceneLevelLoader : MonoBehaviour {
 
     public string levelToLoad;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            SceneManager.LoadScene(levelToLoad);
-
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            Debug.Log("scene one loaded");
-        }
     }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+ 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+            if (other.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene(levelToLoad);
+
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                Debug.Log("scene Master loaded");
+            }
+
+    }
+
+      
 }
