8f5406d25b18d2a8f6dc0e7611ac0a2ce61130d6
BoxParadox <mackanford@hotmail.com>
2018-03-14 18:13:55 +0100

    Nuke works
    I wanna die

--- AetherialGamev2/Assets/Assets/Scripts/Player/Shooting/NukeScript.cs
+++ AetherialGamev2/Assets/Assets/Scripts/Player/Shooting/NukeScript.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 public class NukeScript : MonoBehaviour
 {
     public Transform target;
+    public Transform firePoint;
     private Rigidbody2D rb;
 
     public GameObject NukeExplosionParticle;
     public GameObject NukeSpeedUp;
-    public GameObject NukeTrail;
 
     [Header("Nuke Attributes")]
 
     private float nukeSpeed = 0;
     public float rotateSpeed;
     private float NukeTimer = 0;
+    private float NukeCounter = 0;
 
     void Start ()
     {
@@ -24,28 +25,37 @@ public class NukeScript : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        Vector2 direction = (Vector2)target.position - rb.position;
-        direction.Normalize();
-        float rotateAmount = Vector3.Cross(direction, transform.up).z;
-        rb.angularVelocity = -rotateAmount * rotateSpeed;
-        rb.velocity = transform.up * nukeSpeed;
-
-        NukeTimer += Time.deltaTime;
         IncreaseSpeed();
     }
 
     public void IncreaseSpeed()
     {
+        NukeTimer += Time.deltaTime;
         if (target != null)
         {
-            if (NukeTimer == 1)
+            if (NukeTimer >= 2)
             {
-                nukeSpeed += 10;
-                Instantiate(NukeSpeedUp, transform.position, transform.rotation);
-                Instantiate(NukeTrail, transform.position, transform.rotation);
+                nukeSpeed += 3;
+                NukeCounter++;
+                if (NukeCounter == 1)
+                {
+                    Instantiate(NukeSpeedUp, firePoint.transform.position, transform.rotation);
+                }
             }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null && NukeTimer >= 0)
+        {
+            Vector2 direction = (Vector2)target.position - rb.position;
+            direction.Normalize();
+            float rotateAmount = Vector3.Cross(direction, transform.up).z;
+            rb.angularVelocity = -rotateAmount * rotateSpeed;
+            rb.velocity = transform.up * nukeSpeed;
         }
     }
 
